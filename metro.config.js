a57554807f61109360fe67aa4e97cd71746b24df
taneem77 <mathurtanee07@gmail.com>
2025-09-07 01:47:59 +0530

    Add new folder/files to Lumos project

--- metro.config.js
+++ metro.config.js
@@ -1,30 +1,17 @@
-<<<<<<< HEAD
+// metro.config.js
+
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+
+const defaultConfig = getDefaultConfig(__dirname);
+const {
+  resolver: { assetExts },
+} = defaultConfig;
 
 /**
  * Metro configuration
  * https://reactnative.dev/docs/metro
  *
  * @type {import('@react-native/metro-config').MetroConfig}
- */
-const config = {};
-
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
-=======
-// metro.config.js
-
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
-
-const defaultConfig = getDefaultConfig(__dirname);
-const {
-  resolver: {assetExts},
-} = defaultConfig;
-
-/**
- * Metro configuration
- * https://facebook.github.io/metro/docs/configuration
- *
- * @type {import('metro-config').MetroConfig}
  */
 const config = {
   resolver: {
@@ -34,4 +21,3 @@ const config = {
 };
 
 module.exports = mergeConfig(defaultConfig, config);
->>>>>>> 8b86830102f1727adcd26bd30b9e6ab09f0b43ff
